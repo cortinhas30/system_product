0a757993be1ff98192d0ff0fa94e5fbdf1d3ce38
unknown <lucascortinhas.lc@gmail.com>
2023-05-13 19:09:30 -0300

    conexão com db

--- api/db.js
+++ api/db.js
@@ -1,8 +1,10 @@
 import mysql from "mysql";
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 export const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: process.env.DB,
+    password: process.env.DB_PASS,
     database: "stock",
 });
--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
-require('dotenv').config();
+import userRoutes from "./routes/product.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/", userRoutes);
 
 app.listen(8800);
